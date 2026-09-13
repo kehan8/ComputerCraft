47e6c547e6054d35604e54ba4a1c21de64755bdc
kehan8 <ks.davet@gmail.com>
2026-09-13 18:58:08 +0200

    updated AdminDoor for can use multi location and redstone relay

--- GymArena/AdminDoor/config.lua
+++ GymArena/AdminDoor/config.lua
@@ -5,7 +5,7 @@
 return {
     DETECTOR_NAME = "player_detector_0", -- name of your Player Detector peripheral, checks the door box (see locations.lua)
 
-    DOOR_RELAY_NAME = "redstone_relay_0", -- relay wired to the door
+    DOOR_RELAY_NAME = { "redstone_relay_0" }, -- relay wired to the door
     DOOR_SIDE = "front",                   -- side of that relay driving the door
 
     -- Whitelist of player names allowed through. Anyone else detected nearby
